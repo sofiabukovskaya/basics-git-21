ff36f3813a7d7438cb4dca4e0e6126b52850a65a
Artem Chepurnoy <artemchep@gmail.com>
2019-08-12 11:41:12 +0300

    Show a list of posts

--- app/src/main/java/com/artemchep/basics_git/domain/Post.java
+++ app/src/main/java/com/artemchep/basics_git/domain/Post.java
@@ -1,7 +1,7 @@
 package com.artemchep.basics_git.domain;
 
 public class Post {
-    private final String name;
+    public final String name;
 
     public Post(String name) {
         this.name = name;
